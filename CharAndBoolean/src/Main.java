dcdbc9b7957f7da5560c842e266a03056da121b2
avindhawale <avin.dhawale@gmail.com>
2024-11-16 07:03:06 +0530

    Data types : Char and Boolean

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // char has a width of 16 (2 bytes)
+        char myChar = 'a';
+        System.out.println("MyChar : "+ myChar);
+        char uniChar = '\u00A9';
+        System.out.println("uniChar : "+ uniChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+    }
+}
