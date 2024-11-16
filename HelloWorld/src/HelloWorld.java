f3c827f8a1fcffd3f858f01ee82091a350340d3d
avindhawale <avin.dhawale@gmail.com>
2024-11-16 05:36:47 +0530

    int data type

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello Java");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int total = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(total);
     }
 }
