863dfb7aacd62fa5e5f408546a63753d177f8964
avindhawale <avin.dhawale@gmail.com>
2024-11-16 07:13:36 +0530

    Data Type : String

---
+++ Strings/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+        System.out.println("myString equals to : " + myString);
+
+        int intValue = 50;
+        String stringNumber = "50";
+        System.out.println("Addition of string and int : "+ stringNumber + intValue);
+    }
+}
